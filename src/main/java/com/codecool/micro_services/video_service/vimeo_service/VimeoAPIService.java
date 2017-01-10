abdcc9bdb49df13955b1d60137d406b4b221f3b5
eszterkaloz <eszterkaloz@gmail.com>
2017-01-10 10:13:36 +0100

    More specific function name

--- src/main/java/com/codecool/micro_services/video_service/vimeo_service/VimeoAPIService.java
+++ src/main/java/com/codecool/micro_services/video_service/vimeo_service/VimeoAPIService.java
@@ -5,7 +5,6 @@ import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.thymeleaf.util.StringUtils;
 
 import java.io.IOException;
 import java.net.URI;
@@ -25,10 +24,10 @@ public class VimeoAPIService {
         return INSTANCE;
     }
 
-    public String getVideos(String productName) throws IOException, URISyntaxException {
+    public String getVideosFromVimeo(String productName) throws IOException, URISyntaxException {
         URIBuilder builder = new URIBuilder(API_URL);
-        logger.debug("Getting videos for the following product: {}", productName);
-        logger.debug("The built uri is {}", builder);
+        logger.debug("Getting videos from Vimeo for the following product: {}", productName);
+        logger.debug("The built uri for the Vimepo API is {}", builder);
 
         return execute(builder.build());
     }
