4f23417337bd1f0d94cbdacd9018a9128234c91d
eszterkaloz <eszterkaloz@gmail.com>
2017-01-10 10:37:07 +0100

    Singleton for videoAPIController

--- src/main/java/com/codecool/micro_services/video_service/VideoAPIController.java
+++ src/main/java/com/codecool/micro_services/video_service/VideoAPIController.java
@@ -2,4 +2,13 @@ package com.codecool.micro_services.video_service;
 
 
 public class VideoAPIController {
+
+    private static VideoAPIController INSTANCE;
+
+    public static VideoAPIController getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new VideoAPIController();
+        }
+        return INSTANCE;
+    }
 }
