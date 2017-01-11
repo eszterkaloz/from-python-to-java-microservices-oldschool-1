0c77fcc9dac5974aaa31fab4bcd2e87649551252
Ivanics Szilárd <ivanics.szilard@gmail.com>
2017-01-11 11:37:08 +0100

    Merge branch 'video_service' of github.com:CodecoolBP20161/from-python-to-java-microservices-oldschool-1 into video_service
    # Conflicts:
    #	src/main/java/com/codecool/micro_services/video_service/VideoAPIController.java

--- src/main/java/com/codecool/micro_services/video_service/VideoAPIController.java
+++ src/main/java/com/codecool/micro_services/video_service/VideoAPIController.java
@@ -3,17 +3,13 @@ package com.codecool.micro_services.video_service;
 
 import com.codecool.micro_services.video_service.vimeo_service.VimeoAPIService;
 import com.codecool.micro_services.video_service.youtube_service.YouTubeAPIService;
-
 import org.json.JSONObject;
-
 import spark.Request;
 import spark.Response;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class VideoAPIController {
     public static final String SEARCH_PARAM_KEY = "search";
@@ -28,15 +24,15 @@ public class VideoAPIController {
         this.vimeoAPIService = vimeoAPIService;
     }
 
-    public JSONObject getVideoLinks(Request request, Response response) throws IOException, URISyntaxException {
+    public String getVideoLinks(Request request, Response response) throws IOException, URISyntaxException {
         String searchKey = request.queryParams(SEARCH_PARAM_KEY);
 
         if (searchKey.length() <= 2) {
             response.status(400);
-            JSONObject errorContent = new JSONObject()
+            String errorContent = new JSONObject()
                     .put("error_type", "Bad request. Request parameter is missing or too low?")
-                    .put("error_code", 400);
-            return new JSONObject().put("error", errorContent);
+                    .put("error_code", 400).toString();
+            return errorContent;
         }
 
         videos = new ArrayList<>();
@@ -48,11 +44,24 @@ public class VideoAPIController {
             videos.add(responseBuilder(searchKey, embedCodeForVimeo, "vimeo", category));
         }
 
-        return new JSONObject().put("result", videos);
+        return resultToJSON().toString();
     }
 
-    private String responseBuilder(String title, String embedCode, String provider, String category) {
-        return "{\"title\":\"" + title + "\",\"embed code\":\"" + embedCode + "\",\"provider\":\"" + provider + "\",\"category\":\"" + category + "\"}";
+
+    private String responseBuilder(String title, String embedCode, String provider, String category){
+
+        return  new JSONObject()
+                .put("title", title)
+                .put("embed code", embedCode)
+                .put("provider", provider)
+                .put("category", category).toString();
+        //return "{\"title\":\""+title+"\",\"embed code\":\""+embedCode+"\",\"provider\":\""+provider+"\",\"category\":\""+ category+"\"}";
+    }
+
+    private Map<String, List<String>> resultToJSON() {
+        Map<String, List<String>> result = new HashMap<>();
+        result.put("result", videos);
+        return result;
     }
 
     public String status(Request request, Response response) {
